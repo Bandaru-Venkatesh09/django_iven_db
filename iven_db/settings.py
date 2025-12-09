74161d8dfa50049d49994f81813062e2b0d1d1ab
Bandaru-Venkatesh09 <bandarunarasimha490@gmail.com>
2025-12-09 09:58:15 +0530

    Allow all hosts in settings

--- iven_db/settings.py
+++ iven_db/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-5qczk8)v-mz7o!+l#e(8=+*o&)$hk0!hsap%-508&8c8iktjjo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
