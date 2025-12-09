1bcec8029df82e6a4532a48556d1c42b2c341fe0
Bandaru-Venkatesh09 <bandarunarasimha490@gmail.com>
2025-12-09 09:29:44 +0530

    Fix syntax in start.sh for command execution

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
-!/bin/bash python manage.py collectstatic--noinput 
- python manage.py migrate --noinput gunicorn 
- iven_db.wsgi:application --bind 0.0.0.0:$PORT 
+python manage.py collectstatic --noinput 
+ python manage.py migrate --noinput 
+ gunicorn iven_db.wsgi:application --bind 0.0.0.0:$PORT 
